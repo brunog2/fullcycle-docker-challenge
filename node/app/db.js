790470d2ed0703c307f8766800a8f3b3b9ee75c3
Bruno Gomes <brunofilho2k02@gmail.com>
2025-06-03 15:33:10 -0300

    fix: remove unused log

--- node/app/db.js
+++ node/app/db.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql')
 
-console.log('Database connection config:', {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-})
-
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
